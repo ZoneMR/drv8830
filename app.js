6651e01ac008b9988a7f08ae5d367dbca39d5742
Ladvien <honeysucklehardware@gmail.com>
2018-03-29 11:54:48 +0000

    Finished

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var sleep = require('sleep');
 
 
 // Commands
-const CLEAR_FAULT       = 0x80;
-const FAULT_CMD        = 0x01;
+const FAULT_CMD         = 0x01;
 
 // Fault constants
+const CLEAR_FAULT       = 0x80;
 const FAULT             = 0x01;
 const ILIMIT            = 0x10;
 const OTS               = 0x08;
@@ -16,19 +16,25 @@ const OCP               = 0x02;
 // Direction bits
 const FORWARD           = 0b00000010;
 const REVERSE           = 0b00000001;
-const BRAKE             = 0b00000010;
-const HI_Z              = 0b00000011;
+const HI_Z              = 0b00000000;
+const BRAKE             = 0b00000011;
 
 var address = 0x64;
 
-var getFault = function(address) {
+var getFault = function() {
 
     var fault = {
-        message: "",
+        message: '',
         code: 0
     }
 
-    var faultCode = i2c1.readByteSync(address, FAULT_CMD);
+    var faultCode;
+    try {
+        i2c1.readByteSync(address, FAULT_CMD);
+    } catch (e) {
+        console.log(`Read fault failed: ${e}`)
+    }
+    
     fault.code = faultCode;
 
     if (faultCode !== 0) {
@@ -60,56 +66,89 @@ var getFault = function(address) {
     }
 }
 
-var clearFault = function(address) {
+var clearFault = function() {
     var fault = getFault(address);
     if (fault.code) {
-        var success = i2c1.writeByteSync(address, FAULT_CMD, CLEAR_FAULT);
-        if (success) { return true; }
+        try {
+            var success = i2c1.writeByteSync(address, FAULT_CMD, CLEAR_FAULT);
+            if (success) { return true; }
+        } catch (e) {
+            console.log(`Failed to clear faults: ${e}`)
+        }
     }
     return false;
 }
 
-// var drive = function(address, direction, speed) {
-//     // The speed should be 0-63.
-//     if (speed > 63) { speed = 64; }
-//     if (speed < 0)  { speed =  0; }
-//     console.log(speed.toString(2));
-//     speed = speed << 2 ;
-//     console.log(direction.toString(2));
-//     speed |= direction;
-//     // if (speed < 0) { speed |= 0x01; }
-//     // else           { speed |= 0x02; }
-
-//     console.log(speed.toString(2));
-//     i2c1.writeByteSync(address, 0x00, speed);
-// }
-
-var drive = function(address, speed) {
+var drive = function(speed = 0, direction = undefined, checkFault = false) {
     // The speed should be 0-63.
-    const direction = speed < 0;
-    speed = Math.abs(speed);
-    if (speed > 63) { speed = 63; }
-    speed = speed << 2 ;
-    if (direction) { speed |= 0x01; }
-    else           { speed |= 0x02; }
-    i2c1.writeByteSync(address, 0x00, speed);
+    if (checkFault) { clearFault();}
+    if (direction === undefined) {        
+        direction = speed < 0;
+        speed = Math.abs(speed);
+        if (speed > 63) { speed = 63; }
+        speed = speed << 2 ;
+        if (direction) { speed |= FORWARD; }
+        else           { speed |= REVERSE; }
+    } else {
+        speed = speed << 2 ;
+        speed |= direction;
+    }
+    try {
+        i2c1.writeByteSync(address, 0x00, speed);
+    } catch (e){
+        console.log('Drive command failed.')
+    }
 }
 
-var brake = function(address) {}
+var brake = function() {
+    try {
+        drive(0, HI_Z);
+    } catch (e) {
+        console.log('Brake command failed.')
+    }
+}
 
-console.log(getFault(address).message);
-var success = clearFault(address);
 
+var stop = function() {
+    try {
+        drive(0, BRAKE);
+    } catch (e) {
+        console.log('Brake command failed.')
+    }
+}
 
-for (var i = 0; i < 64; i++){
-    drive(address, i);
+console.log(getFault().message);
+var success = clearFault();
+
+for (var i = 0; i < 63; i++){
+    drive(i);
+    sleep.msleep(50);
+}
+sleep.msleep(500);
+for (var i = 63; i > 0; i--){
+    drive(i);
     sleep.msleep(50);
 }
 
-for (var i = 64; i > 0; i--){
-    drive(address, i);
-    sleep.msleep(50);
-}
+// Drive forward full speed.
+drive(63);
+sleep.sleep(1);
+// Hold idle
+drive(0, HI_Z);
+sleep.sleep(1);
+// Drive reverse full speed.
+drive(-63);
+sleep.sleep(1);
 
+// Brake, but continue forward.
+brake(50);
+sleep.sleep(1);
 
-drive(address, BRAKE, 0);
+// Drive forward, but clear faults first.
+drive(50, undefined, true);
+sleep.sleep(1);
+
+// Stop.
+drive(0, BRAKE);
+// OR
+stop();
